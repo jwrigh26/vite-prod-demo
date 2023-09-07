3350a77025810d0d29c53f3091f8494c61ad91eb
jwrigh26 <jwrigh26@gmail.com>
2023-09-06 22:08:38 -0600

    Added eslint, prettier, jsconfig, etc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,98 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: {
+    react: { version: '18.2' },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react-hooks/exhaustive-deps': 'off', // Devs can figure out what to pass to useEffect.
+    // NOT SURE NEEDED
+    // Allow us to never have to import React
+    // 'react/jsx-uses-react': 'off',
+    // 'react/react-in-jsx-scope': 'off',
+    // --------- OTHER RULES ---------
+
+    // React fules
+    'react/jsx-filename-extension': 'on', // We use the JSX extension
+    'react/forbid-prop-types': 'off',
+    'react/jsx-no-bind': 'on',
+    'react/require-default-props': 'off',
+    'react/jsx-fragments': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/no-unescaped-entities': 'on',
+    'react/jsx-wrap-multilines': 'off',
+    'react/jsx-props-no-spreading': 'off',
+
+    // My rules
+    'no-plusplus': 'on',
+    'no-nested-ternary': 'on',
+    'no-confusing-arrow': 'off', // I'll be the judge of that is confusing ;)
+    'jsx-a11y/anchor-is-valid': 'off', // TODO: revisit
+    'jsx-a11y/iframe-has-title': 'off',
+    'no-return-await': 'off',
+    'no-underscore-dangle': 'off',
+
+    // handled by prettier
+    indent: 'off',
+    'object-curly-spacing': 'off',
+    'comma-dangle': 'off',
+    'object-curly-newline': 'off',
+    'generator-star-spacing': 'off',
+    'react/jsx-curly-newline': 'off',
+    'function-paren-newline': 'off',
+    'array-bracket-spacing': 'off',
+    quotes: 'off',
+    'space-before-function-paren': 'off',
+    'react/jsx-indent': 'off',
+    semi: 'off',
+
+    // show as warnings
+    'prefer-template': 'warn',
+    'object-shorthand': 'warn',
+    'react/jsx-boolean-value': 'warn',
+    'react/jsx-curly-brace-presence': 'warn',
+    'react/no-array-index-key': 'warn',
+    'prefer-destructuring': 'warn',
+    'react/destructuring-assignment': 'warn',
+    'no-param-reassign': 'off', // TODO: turn this to "warn" after figuring out how to exclude slice files.
+    'no-unused-vars': 'off', // disallow declaration of variables that are not used in the code
+    'no-useless-computed-key': 'off',
+    'import/no-named-as-default': 'off',
+    'import/no-named-as-default-member': 'off',
+    'max-len': [
+      2,
+      {
+        code: 80,
+        ignoreComments: true,
+        ignoreUrls: true,
+        ignoreTrailingComments: true,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignorePattern: '^import .*',
+      },
+    ], // specify the maximum length of a line in your program (off by default)
+    // MUI
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['@mui/*/*/*'],
+      },
+    ],
   },
-}
+};
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: false, // Disabling esbuild, since we'll be using swc
+  swc: {
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
+    // Additional SWC options can be provided here as needed.
+  },
+  server: {
+    port: 9090, // will be used if available, or the next free one if not
+  },
+  build: {
+    emptyOutDir: true,
+    sourcemap: true,
+    minify: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+    target: 'es2018',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      assets: path.resolve(__dirname, 'src/assets'),
+      components: path.resolve(__dirname, 'src/components'),
+      constants: path.resolve(__dirname, 'src/constants'),
+      context: path.resolve(__dirname, 'src/context'),
+      helpers: path.resolve(__dirname, 'src/helpers'),
+      hooks: path.resolve(__dirname, 'src/hooks'),
+      layouts: path.resolve(__dirname, 'src/layouts'),
+      models: path.resolve(__dirname, 'src/models'),
+      modules: path.resolve(__dirname, 'src/modules'),
+      providers: path.resolve(__dirname, 'src/providers'),
+      services: path.resolve(__dirname, 'src/services'),
+      styles: path.resolve(__dirname, 'src/styles'),
+      src: path.resolve(__dirname, 'src'),
+      store: path.resolve(__dirname, 'src/store'),
+      validation: path.resolve(__dirname, 'src/validation'),
+      React: path.resolve(__dirname, 'node_modules/react/'),
+    },
+  },
+});
